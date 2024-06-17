1867c1fb8bcbf18998a4c96104af0d74244144f8
darshan <darshanchovatiya31@gmail.com>
2024-06-17 12:11:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -21,45 +21,61 @@ import Protected from "./component/pages/protected/Protected";
 import { useEffect, useState } from "react";
 import { CratContext } from "./component/Context/CartContext";
 import Shopeditem from "./component/pages/shop single/Shopeditem";
-import AOS from 'aos';
-import 'aos/dist/aos.css'; 
+import AOS from "aos";
+import "aos/dist/aos.css";
 AOS.init();
 
 function App() {
   const [cart, setCart] = useState(0);
   const [cartItem, setCartItem] = useState([]);
+  const [Spinner, setSpinner] = useState(false);
 
   useEffect(() => {
     let a = localStorage.getItem("cartnumber");
     setCart(parseInt(a === null ? 0 : a));
   }, [cart]);
 
+  {
+    setTimeout(() => {
+      setSpinner(true);
+    }, 1000);
+  }
   return (
-    <CratContext.Provider value={{ cart: { cart, setCart }, cartitems: { cartItem, setCartItem } }}>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/shopsingle" element={<Shopsingle />} />
-          <Route path="/service" element={<Service />} />
-          <Route path="/servicesingle" element={<Servicesingle />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/portfoliosingle" element={<Portfoliosingle />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blogsingle" element={<Blogsingle />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<Error />} />
-          <Route path="licenses" element={<Licenses />} />
-          <Route path="changelog" element={<Changelog />} />
-          <Route path="protected" element={<Protected />} />
-          <Route path="shopeditem" element={<Shopeditem/>}/>
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </CratContext.Provider>
+    <>
+      {Spinner && (
+        <CratContext.Provider
+          value={{
+            cart: { cart, setCart },
+            cartitems: { cartItem, setCartItem },
+          }}
+        >
+          <BrowserRouter>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/shop" element={<Shop />} />
+              <Route path="/shopsingle" element={<Shopsingle />} />
+              <Route path="/service" element={<Service />} />
+              <Route path="/servicesingle" element={<Servicesingle />} />
+              <Route path="/portfolio" element={<Portfolio />} />
+              <Route path="/portfoliosingle" element={<Portfoliosingle />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/blog" element={<Blog />} />
+              <Route path="/blogsingle" element={<Blogsingle />} />
+              <Route path="contact" element={<Contact />} />
+              <Route path="*" element={<Error />} />
+              <Route path="licenses" element={<Licenses />} />
+              <Route path="changelog" element={<Changelog />} />
+              <Route path="protected" element={<Protected />} />
+              <Route path="shopeditem" element={<Shopeditem />} />
+            </Routes>
+            <Footer />
+          </BrowserRouter>
+        </CratContext.Provider>
+      )}
+      {!Spinner &&<div className="main_loader d-flex justify-content-center align-items-center"><p></p></div>}
+    </>
   );
 }
 export default App;
